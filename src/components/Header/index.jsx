25d9771241c7689ef92708636c8c9404522848ce
TrikMary <shanmarishka@gmail.com>
2024-07-25 21:56:57 +0300

    Поправила по замечаниям к дз

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,22 @@
 import { useState } from "react";
 
-export const Header = ({addCard}) => {
-  {/* Создаем состояние isOpen, которое отвечает открыто или нет окошко и */}
-  {/* которое меняется с помощью функции */}
-  {/* setIsOpen с первоначальным значение false */}
-  const [isOpen, setIsOpen] = useState (false)
-  {/*Создаем функцию в которой меняется состояние на противоположное */}
-  const toggleOpenUser = () => {
-    setIsOpen(!isOpen)
+export const Header = ({ addCard }) => {
+  {
+    /* Создаем состояние isOpen, которое отвечает открыто или нет окошко и */
   }
+  {
+    /* которое меняется с помощью функции */
+  }
+  {
+    /* setIsOpen с первоначальным значение false */
+  }
+  const [isOpen, setIsOpen] = useState(false);
+  {
+    /*Создаем функцию в которой меняется состояние на противоположное */
+  }
+  const toggleOpenUser = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <header className="header">
       <div className="container">
@@ -24,32 +32,34 @@ export const Header = ({addCard}) => {
             </a>
           </div>
           <nav className="header__nav">
-            <button onClick={addCard} className="header__btn-main-new _hover01" id="btnMainNew">
-              <a >Создать новую задачу</a>
+            <button
+              onClick={addCard}
+              className="header__btn-main-new _hover01"
+              id="btnMainNew"
+            >
+              <a>Создать новую задачу</a>
             </button>
             {/* ---тут код для выскакивающего элемента пользователя, добавляем onClick, который вызывает функцию--- */}
-            <a className="header__user _hover02" onClick={toggleOpenUser} >
+            <a className="header__user _hover02" onClick={toggleOpenUser}>
               Ivan Ivanov
             </a>
-           {/*Пишем логику для отрисовки модального окошка, если первое условие выполнено, то сработает второе*/}
-           {isOpen && (
-            <div
-            className="header__pop-user-set pop-user-set"
-            id="user-set-target"
-          >
-             {/* <a href="">x</a> */} 
-            <p className="pop-user-set__name">Ivan Ivanov</p>
-            <p className="pop-user-set__mail">ivan.ivanov@gmail.com</p>
-            <div className="pop-user-set__theme">
-              <p>Темная тема</p>
-              <input type="checkbox" className="checkbox" name="checkbox" />
-            </div>
-            <button type="button" className="_hover03">
-              <a href="#popExit">Выйти</a>
-            </button>
-          </div>
-           )}
-            
+            {/*Пишем логику для отрисовки модального окошка, если первое условие выполнено, то сработает второе*/}
+            {isOpen && (
+              <div
+                className="header__pop-user-set pop-user-set"
+                id="user-set-target"
+              >
+                <p className="pop-user-set__name">Ivan Ivanov</p>
+                <p className="pop-user-set__mail">ivan.ivanov@gmail.com</p>
+                <div className="pop-user-set__theme">
+                  <p>Темная тема</p>
+                  <input type="checkbox" className="checkbox" name="checkbox" />
+                </div>
+                <button type="button" className="_hover03">
+                  <a href="#popExit">Выйти</a>
+                </button>
+              </div>
+            )}
           </nav>
         </div>
       </div>
