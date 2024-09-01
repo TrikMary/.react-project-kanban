d7ee117a9b852e6c2e3442bae14f4107acbb4d05
TrikMary <shanmarishka@gmail.com>
2024-09-01 15:18:54 +0300

    создан скелет страниц входа и регистрации

--- src/router/AppRoutes.jsx
+++ src/router/AppRoutes.jsx
@@ -1,8 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { routes } from "./routes.js"
-import { NotFound } from "../pages/NotFound/NotFound.jsx"
+import { NotFound } from "../pages/NotFoundPage/NotFoundPage.jsx"
 import { MainPage } from "../pages/MainPage/MainPage.jsx"
-import { Login } from "../pages/Login/Login.jsx"
+
+import { LoginPage } from "../pages/LoginPage/LoginPage.jsx"
+import { RegisterPage } from "../pages/RegisterPage/RegisterPage.jsx"
 
 export const AppRoutes = ({ changeTheme, setChangeTheme }) => {
     return (
@@ -10,7 +12,8 @@ export const AppRoutes = ({ changeTheme, setChangeTheme }) => {
             <Routes>
                 <Route path={routes.main} element={<MainPage changeTheme={changeTheme} setChangeTheme={setChangeTheme} />} />
                 <Route path={routes.notFound} element={<NotFound />} />
-                <Route path={routes.login} element={<Login />} />
+                <Route path={routes.login} element={<LoginPage />} />
+                <Route path={routes.register} element={<RegisterPage />} />
             </Routes>
         </BrowserRouter>
     )
