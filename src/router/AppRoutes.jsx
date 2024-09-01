3a4622d99f54e8a3cda07e51f24722b81544c253
TrikMary <shanmarishka@gmail.com>
2024-09-01 03:05:52 +0300

    создан набросок страницы login

--- src/router/AppRoutes.jsx
+++ src/router/AppRoutes.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { routes } from "./routes.js"
 import { NotFound } from "../pages/NotFound/NotFound.jsx"
 import { MainPage } from "../pages/MainPage/MainPage.jsx"
+import { Login } from "../pages/Login/Login.jsx"
 
 export const AppRoutes = ({ changeTheme, setChangeTheme }) => {
     return (
@@ -9,7 +10,7 @@ export const AppRoutes = ({ changeTheme, setChangeTheme }) => {
             <Routes>
                 <Route path={routes.main} element={<MainPage changeTheme={changeTheme} setChangeTheme={setChangeTheme} />} />
                 <Route path={routes.notFound} element={<NotFound />} />
-                
+                <Route path={routes.login} element={<Login />} />
             </Routes>
         </BrowserRouter>
     )
