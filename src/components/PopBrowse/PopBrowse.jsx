d270a1b260662308ee0bcd88fbda24ffaef84f50
TrikMary <shanmarishka@gmail.com>
2024-09-04 23:54:14 +0300

    выполнены все задания по чек листу

--- src/components/PopBrowse/PopBrowse.jsx
+++ src/components/PopBrowse/PopBrowse.jsx
@@ -10,7 +10,7 @@ export const PopBrowse = () => {
         <div className="pop-browse__block">
           <div className="pop-browse__content">
             <div className="pop-browse__top-block">
-              <h3 className="pop-browse__ttl">Название задачи</h3>
+              <h3 className="pop-browse__ttl">Название задачи </h3>
               <div className="categories__theme theme-top _orange _active-category">
                 <p className="_orange">Web Design</p>
               </div>
