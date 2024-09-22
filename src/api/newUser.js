b87131766fb0975c142413e87a8aa1c8eb7bda2d
TrikMary <shanmarishka@gmail.com>
2024-09-23 00:29:31 +0300

    рабочая регистрация

--- src/api/newUser.js
+++ src/api/newUser.js
@@ -1,14 +1,14 @@
 
 const apiURL = "https://wedev-api.sky.pro/api/user";
-export const registerUser = async ({login, password}) => {
+export const registerUser = async ({name, login, password}) => {
   const response = await fetch(apiURL, {
     method: "POST",
     body: JSON.stringify ({
-        login, password
+        name, login, password
     })
   });
    if (response.status === 400) {
-    throw new Error ("Неверный логин/пароль")
+    throw new Error ("Такой пользователь уже существует")
   };
   
   if (!response.ok) {
--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,15 +1,37 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { routes } from "../../router/routes.js"
 import * as S from "./registerPage.styled.js"
+import { registerUser } from "../../api/newUser.js";
+import { useState } from "react";
 
 
 
-export const RegisterPage = ({ setIsUser }) => {
+export const RegisterPage = () => {
 // Функция имитирующая регистрацию
-  const registerTrue = () => {
-    
-    setIsAuth(true)
-        
+  const [formData, setFormData] = useState({name:"", login:"", password:""});
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    if (formData.name === ""){
+      setError("Введите имя")
+      return
+    }
+    if (formData.login === ""){
+      setError("Введите логин")
+      return
+    }
+    if (formData.password === ""){
+      setError("Введите пароль")
+      return
+    }
+    registerUser(formData).then((res) => {
+      console.log(res);
+      navigate(routes.login)
+    });
+    // setIsAuth(true)
+
   }
     return (
       <S.RegPageGlobalStyle>
@@ -20,27 +42,32 @@ export const RegisterPage = ({ setIsUser }) => {
                 <S.ModalTtl>
                   <h2>Регистрация</h2>
                 </S.ModalTtl>
-                <S.ModalFormReg id="formLogUp" action="#">
-                  <S.ModalInput            
+                <S.ModalFormReg id="formLogUp" action="#" onSubmit={handleRegister}>
+                  <S.ModalInput
+                    onChange={(e) => setFormData({...formData, name:e.target.value})}            
                     type="text"
                     name="first-name"
                     id="first-name"
                     placeholder="Имя"
                   />
-                  <S.ModalInput            
+                  <S.ModalInput 
+                    onChange={(e) => setFormData({...formData, login:e.target.value})}            
                     type="text"
                     name="login"
                     id="loginReg"
                     placeholder="Эл. почта"
                   />
-                  <S.ModalInput            
+                  <S.ModalInput
+                    onChange={(e) => setFormData({...formData, password:e.target.value})}             
                     type="password"
                     name="password"
                     id="passwordFirst"
                     placeholder="Пароль"
                   />
-                  <S.ModalBtnSignupEnt id="SignUpEnter">
-                    <Link onClick={registerTrue} to={routes.login}>Зарегистрироваться</Link>{" "}
+                  {error && <p>{error}</p>}
+                  <S.ModalBtnSignupEnt id="SignUpEnter" type="submit">
+                    
+                    Зарегистрироваться
                   </S.ModalBtnSignupEnt>
                   <S.ModalFormGroup >
                     <p>
